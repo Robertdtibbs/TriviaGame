82b076a6176a12f67d65b2232f76e16f0c760c7f
Teb Mosby <tibbsr09@gmail.com>
2019-03-07 22:23:26 -0500

    finalized questions and answers, was able to add buttoms and started more psuedo code on game play funcitonality.

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -1,30 +1,105 @@
 
 
-var questionGroup ={
-    // questionAsia = "What is the largest country in Asia?",
-    // questionAsia2 = "Which country is partially located in Europe?",
-    // questionEurope = "Name of the mountain range that runs through Europe?",
-    // questionEurope2= "Which European city is further south?",
-    // questionSouthAm = "How many countries are located in South America?",
-    // questionSouthAm2= "What is the largest city in South America?",
-    // questionNorthAm = "Name the largest mountain in North America?",
-    // questionNorthAm2 = "What is the longest river in North America?",
-    // questionAfrica = "What is the total distance from the top of Afica to the bottom?",
-    // questionAfica2 = "How many different languages are spoken across Africa?",
-}
+var questionGroup =[
+    {
+        question: "What is the largest country in Asia?",
+        answerList: ["China", "India", "Japan", "Thailand"],
+        answer: "China"
+    
+    },
+    {
+        question: "Which country is partially located in Europe?",
+        answerList: ["Cambodia", "India", "Russia", "Vietnam"],
+        answer: "Russia"
+    },
+    {
+        question: "Name of the mountain range that runs through Europe?",   
+        answerList: ["Andes", "Rocky", "Appalachain", "Alps"],
+        answer: "Alps"
+    
+    },
+    {
+        question: "Which European city is further south?",
+        answerList: ["Paris", "Geneva", "Oslo", "Rome"],
+        answer: "Rome"
+    
+    },
+    {
+        question: "How many countries are located in South America?",
+        answerList: ["12", "14", "8", "11"],
+        answer: "12"
+    
+    },
+    {
+        question: "What is the largest city in South America?",
+        answerList: ["Chile", "Argentina", "Brazil", "Columbia"],
+        answer: "Brazil"
+    
+    },
+    {
+        question: "Name the largest mountain in North America?",
+        answerList: ["Denali", "Loveland", "Hood", "Lucania"],
+        answer: "Denali"
+    
+    },
+    {
+        question: "What is the longest river in North America?",
+        answerList: ["Mississippi", "Missouri", "Colorado", "Columbia"],
+        answer: "Mississippi"
+    
+    },
+    {
+        question: "What is the total distance from the top of Afica to the bottom?",
+        answerList: ["Namib", "Kalahari", "Sahara", "Karoo"],
+        answer: "Sahara"
+    
+    },
+    {
+        question: "How many different languages are spoken across Africa?",
+        answerList: ["50", "100", "150", "1500"],
+        answer: "1500"
+    
+    }
+];
 
-var correctAnswers = "";
-var wrongAnswers= "";
+
 
 
 window.onload = function(){
     var timer = 30;
+    var correctAnswers = 0;
+    var wrongAnswers= 0;
+
     function clock(){
         timer--;
         $("#timer").text("You have " + timer +" seconds.");
+        
     };
     setInterval(clock, 1000);
     $("#timer").text("You have " + timer +" seconds.");
-    $("#questions").text("question");
-    $("#answers").text("answers");
+
+   
+    // $("#answers").text("answers");
+    for(var i = 0; i < questionGroup.length; i++){
+        console.log(questionGroup[i].question);
+        $("#questions").text(questionGroup[i].question);
+        var q1 = $("<button>").text(questionGroup[i].answerList[i]);
+        console.log(questionGroup[i].answerList[i]);
+        // var q2 = $("<button>").text(questionGroup[i].answerList);
+        // var q3 = $("<button>").text(questionGroup[i].answerList);
+        // var q4 = $("<button>").text(questionGroup[i].answerList);
+        // answers.append(q1);
+        $("#answers").appendchild(q1);
+        // function userGuess(){
+
+        // };
+
+
+        // if(timer === 0){
+        //     $("#questions").text("Out of time!");
+        // };
+
+
+
+    }
 }
