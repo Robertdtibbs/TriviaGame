9aa51962be4a70db76bb04efd8d5c9e628b8566b
Teb Mosby <tibbsr09@gmail.com>
2019-03-14 20:45:05 -0400

    removed excess code and added comments for easy review

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -84,86 +84,52 @@ var counter = 0;
 var correctAnswers = 0;
 var wrongAnswers= 0;
 var timer = 15;
+var clockRunning = false;
+var intervalID
 
 console.log(questionGroup.length);
 
-function clock(){
-    timer--;
-    $("#timer").text("You have " + timer +" seconds.");
-        if(timer < 1){
-            clearTimeout(timer);
-            $("#timer").text("Times Up! You lost");
-            $("#questions").text(" ");
-            $("#answers").text(" ");
-            $("#pictures").html("<img src = assests/images/m8hfb3.jpg />");
-            setTimeout(completeReset,7000);
-            // wrongAnswers++;
-            // $("#answers").text(" ");
-            // $("#pictures").html("<img src = '" + questionGroup[counter].image + "'/>")
-            // pause = setInterval(questionGenReset, 3000);
-            // counter++;
-        };
-    
+
+// officially start internal count down of running clock
+function startTimer (){
+    if(!clockRunning){
+        intervalID = setInterval(countDown, 1000);
+        clockRunning = true;
+    }
 };
-go = setInterval(clock, 1000);
-$("#timer").text("You have " + timer +" seconds.");
 
+// stops timer after quesiton is answered
+function stopTimer(){
+    clearInterval(intervalID);
+    clockRunning = false;
+};
 
+// places time in the Dom and creates a if statment to display image if question is not answered in time
+function countDown() {
+    $("#timer").text("You have " +timer+ " seconds");
+    timer --;
+
+    if (timer === 0) {
+        $("#timer").text("You've run out of time!");
+        $("#answers").empty();
+        $("#questions").empty();
+        $("#pictures").html("<img src = assests/images/m8hfb3.jpg />");
+        counter++;
+        wrongAnswers++;
+        stopTimer();
+        setTimeout(questionGen, 3000);
+    }
+}
+
+// function pulls quesitons from array of groups and places them in the DOM as clickable answers.
+// if and else statements decide if the user is right based on value assigned to each potential answer
+// image will show of correct answer and the game will let you know if you answer correct or incorrectly.
+// set timeout pauses the game to reflect on answer before reseting to the next questing when the counter increases,
 function questionGen() {
-    console.log(questionGroup[counter].question);
-    $("#questions").text(questionGroup[counter].question);
-    var q1 = $("<button>").text(questionGroup[counter].answerList[0]);
-    q1.data("value", 0);
-    var q2 = $("<button>").text(questionGroup[counter].answerList[1]);
-    q2.data("value", 1);
-    var q3 = $("<button>").text(questionGroup[counter].answerList[2]);
-    q3.data("value", 2);
-    var q4 = $("<button>").text(questionGroup[counter].answerList[3]);
-    q4.data("value", 3);
-   
 
-    console.log(questionGroup[counter].answerList);
-    // var answers = questionGroup[counter].answerList;
-    // answers.append(q1);
-    $("#answers").append(q1);
-    $("#answers").append(q2);
-    $("#answers").append(q3);
-    $("#answers").append(q4);
-
-    $("button").on("click", function() {
-        var userChoice = ($(this).data("value"));
-        console.log(userChoice);
-        console.log(questionGroup[counter].answer)
-        if(userChoice === questionGroup[counter].answer){
-            correctAnswers ++;
-            
-            $("#questions").text("Right Answer!");
-            $("#answers").text(" ");
-            $("#pictures").html("<img src = '" + questionGroup[counter].image + "'/>")
-            pause = setInterval(questionGenReset, 3000);
-            // clearInterval(go);
-            counter ++;
-            
-        
-    
-        } else{
-            wrongAnswers++;
-            
-            $("#questions").text("Wrong Answer! The answer is " + questionGroup[counter].answerText);
-            $("#answers").text(" ");
-            $("#pictures").html("<img src = '" + questionGroup[counter].image + "'/>")
-            pause = setInterval(questionGenReset, 3000);
-            // clearInterval(go);
-            counter++;
-        }
-    });
-
-};
-
-function questionGenReset() {
-    clearInterval(pause);
     timer = 15
-    clock();
+    startTimer();
+    $("#answers").empty();
     console.log(questionGroup[counter].question);
     $("#questions").text(questionGroup[counter].question);
     var q1 = $("<button>").text(questionGroup[counter].answerList[0]);
@@ -178,8 +144,6 @@ function questionGenReset() {
     console.log(q1);
 
     console.log(questionGroup[counter].answerList);
-    // var answers = questionGroup[counter].answerList;
-    // answers.append(q1);
     $("#answers").append(q1);
     $("#answers").append(q2);
     $("#answers").append(q3);
@@ -197,16 +161,16 @@ function questionGenReset() {
             $("#questions").text("Right Answer!");
             $("#answers").text(" ");
             $("#pictures").html("<img src = '" + questionGroup[counter].image + "'/>")
-            pause = setInterval(questionGenReset, 3000);
-            // clearInterval(go);
+            stopTimer();
+            setTimeout(questionGen,5000);
             counter ++;
         } else{
             wrongAnswers++;
             $("#questions").text("Wrong Answer! The answer is " + questionGroup[counter].answerText);
             $("#answers").text(" ");
             $("#pictures").html("<img src = '" + questionGroup[counter].image + "'/>")
-            pause = setInterval(questionGenReset, 3000);
-            // clearInterval(go);
+            stopTimer();
+            setTimeout(questionGen,5000);
             counter++;
         }
         if(counter===10){
@@ -218,7 +182,7 @@ function questionGenReset() {
 
 function endGame() {
 
-    clearInterval(go)
+    stopTimer();
 
     $("#timer").empty();                         
     $("#questions").empty();                          
@@ -239,256 +203,11 @@ function endGame() {
     timer = 15;
     counter = 0;
 
-    setTimeout(completeReset,7000);
+    setTimeout(questionGen,7000);
 }
 
 
 window.onload = function(){
     questionGen();
-    clock();
 } 
 
-function completeReset(){
-    var questionGroup =[
-        {
-            question: "What is the largest country in Asia?",
-            answerList: ["China", "India", "Japan", "Thailand",],
-            answer: 0,
-            answerText: "China",
-            image: src = "assests/images/china.jpg",
-        
-        },
-        {
-            question: "Which country is partially located in Europe?",
-            answerList: ["Cambodia", "India", "Russia", "Vietnam",],
-            answer: 2,
-            answerText: "Russia",
-            image: "assests/images/Russia_Moscow.jpg",
-        },
-        {
-            question: "Name of the mountain range that runs through Europe?",   
-            answerList: ["Andes", "Rocky", "Appalachain", "Alps",],
-            answer: 3,
-            answerText: "Alps",
-            image: "assests/images/alps.jpg",
-        
-        },
-        {
-            question: "Which European city is further south?",
-            answerList: ["Paris", "Geneva", "Oslo", "Rome",],
-            answer: 3,
-            answerText: "Rome",
-            image: "assests/images/colisee-rome.jpg",
-        
-        },
-        {
-            question: "How many countries are located in South America?",
-            answerList: ["12", "14", "8", "11",],
-            answer: 0,
-            answerText: "12",
-            image: "assests/images/southamerica.jpg",
-        
-        },
-        {
-            question: "What is the largest county in South America?",
-            answerList: ["Chile", "Argentina", "Brazil", "Columbia",],
-            answer: 2,
-            answerText: "Brazil",
-            image: "assests/images/Rio.jpg",
-        
-        },
-        {
-            question: "Name the largest mountain in North America?",
-            answerList: ["Denali", "Loveland", "Hood", "Lucania",],
-            answer: 0,
-            answerText: "Denali",
-            image: "assests/images/denali-national-park-preserve.jpg",
-        
-        },
-        {
-            question: "What is the longest river in North America?",
-            answerList: ["Mississippi", "Missouri", "Colorado", "Columbia",],
-            answer: 0,
-            answerText: "Mississippi",
-            image: "assests/images/mississippi.jpg",
-        
-        },
-        {
-            question: "What is the larget desert in Africa?",
-            answerList: ["Namib", "Kalahari", "Sahara", "Karoo",],
-            answer: 2,
-            answerText: "Sahara",
-            image: "assests/images/SaharaDesert.jpg",
-        
-        },
-        {
-            question: "How many different languages are spoken across Africa?",
-            answerList: ["50", "100", "150", "1,500",],
-            answer: 3,
-            answerText: "1,500",
-            image: "assests/images/africa-cruise-safari.jpg",
-        
-        }
-    ];
-    var counter = 0;
-    var correctAnswers = 0;
-    var wrongAnswers= 0;
-    var timer = 15;
-    
-    console.log(questionGroup.length);
-    
-    function clock(){
-        timer--;
-        $("#timer").text("You have " + timer +" seconds.");
-            if(timer < 1){
-                clearTimeout(timer);
-                $("#timer").text("Times Up! You lost");
-                $("#questions").text(" ");
-                $("#answers").text(" ");
-                $("#pictures").html("<img src = assests/images/m8hfb3.jpg />");
-                setTimeout(completeReset,7000);
-                // wrongAnswers++;
-                // $("#answers").text(" ");
-                // $("#pictures").html("<img src = '" + questionGroup[counter].image + "'/>")
-                // pause = setInterval(questionGenReset, 3000);
-                // counter++;
-            };
-        
-    };
-    go = setInterval(clock, 1000);
-    $("#timer").text("You have " + timer +" seconds.");
-    
-    
-    function questionGen() {
-        console.log(questionGroup[counter].question);
-        $("#questions").text(questionGroup[counter].question);
-        var q1 = $("<button>").text(questionGroup[counter].answerList[0]);
-        q1.data("value", 0);
-        var q2 = $("<button>").text(questionGroup[counter].answerList[1]);
-        q2.data("value", 1);
-        var q3 = $("<button>").text(questionGroup[counter].answerList[2]);
-        q3.data("value", 2);
-        var q4 = $("<button>").text(questionGroup[counter].answerList[3]);
-        q4.data("value", 3);
-       
-    
-        console.log(questionGroup[counter].answerList);
-        // var answers = questionGroup[counter].answerList;
-        // answers.append(q1);
-        $("#answers").append(q1);
-        $("#answers").append(q2);
-        $("#answers").append(q3);
-        $("#answers").append(q4);
-    
-        $("button").on("click", function() {
-            var userChoice = ($(this).data("value"));
-            console.log(userChoice);
-            console.log(questionGroup[counter].answer)
-            if(userChoice === questionGroup[counter].answer){
-                correctAnswers ++;
-                
-                $("#questions").text("Right Answer!");
-                $("#answers").text(" ");
-                $("#pictures").html("<img src = '" + questionGroup[counter].image + "'/>")
-                pause = setInterval(questionGenReset, 3000);
-                // clearInterval(go);
-                counter ++;
-                
-            
-        
-            } else{
-                wrongAnswers++;
-                
-                $("#questions").text("Wrong Answer! The answer is " + questionGroup[counter].answerText);
-                $("#answers").text(" ");
-                $("#pictures").html("<img src = '" + questionGroup[counter].image + "'/>")
-                pause = setInterval(questionGenReset, 3000);
-                // clearInterval(go);
-                counter++;
-            }
-        });
-    
-    };
-    
-    function questionGenReset() {
-        clearInterval(pause);
-        timer = 15
-        clock();
-        console.log(questionGroup[counter].question);
-        $("#questions").text(questionGroup[counter].question);
-        var q1 = $("<button>").text(questionGroup[counter].answerList[0]);
-        q1.data("value", 0);
-        var q2 = $("<button>").text(questionGroup[counter].answerList[1]);
-        q2.data("value", 1);
-        var q3 = $("<button>").text(questionGroup[counter].answerList[2]);
-        q3.data("value", 2);
-        var q4 = $("<button>").text(questionGroup[counter].answerList[3]);
-        q4.data("value", 3);
-       
-        console.log(q1);
-    
-        console.log(questionGroup[counter].answerList);
-        // var answers = questionGroup[counter].answerList;
-        // answers.append(q1);
-        $("#answers").append(q1);
-        $("#answers").append(q2);
-        $("#answers").append(q3);
-        $("#answers").append(q4);
-        $("#pictures").text(" ");
-    
-        $("button").on("click", function() {
-            var userChoice = ($(this).data("value"));
-            console.log(userChoice);
-            console.log(questionGroup[counter].answer)
-            if(counter === 10){
-               endGame();
-            } else if(userChoice === questionGroup[counter].answer){
-                correctAnswers ++;
-                $("#questions").text("Right Answer!");
-                $("#answers").text(" ");
-                $("#pictures").html("<img src = '" + questionGroup[counter].image + "'/>")
-                pause = setInterval(questionGenReset, 3000);
-                // clearInterval(go);
-                counter ++;
-            } else{
-                wrongAnswers++;
-                $("#questions").text("Wrong Answer! The answer is " + questionGroup[counter].answerText);
-                $("#answers").text(" ");
-                $("#pictures").html("<img src = '" + questionGroup[counter].image + "'/>")
-                pause = setInterval(questionGenReset, 3000);
-                // clearInterval(go);
-                counter++;
-            }
-            if(counter===10){
-                endGame();
-            }
-        });  
-    
-    };
-    
-    function endGame() {
-    
-        clearInterval(go)
-    
-        $("#timer").empty();                         
-        $("#questions").empty();                          
-        $("#answers").empty();
-        $("#pictures").empty();
-    
-        $("#timer").text("Let's see how you did!")
-        $("#questions").text("Correct: " +correctAnswers +"   Incorrect: " +wrongAnswers);
-        if(correctAnswers > wrongAnswers){
-            $("#pictures").html("<img src = assests/images/superstar.jpg />");
-        }else{
-            $("#pictures").html("<img src = assests/images/do-better.jpg />");
-        };
-        
-    
-        correctAnswers = 0;                                      
-        wrongAnswers = 0;
-        timer = 30;
-        counter = 0;
-    
-        setTimeout(completeReset,7000);
-    }
-};
